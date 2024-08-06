eec26e898dbe144c05f1b9eb0041a19cba7eeaef
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-08-06 21:17:08 +0800

    migrations for requirement and requirement submission

--- database/migrations/2024_08_06_131218_create_requirements_table.php
+++ database/migrations/2024_08_06_131218_create_requirements_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('requirements', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('status');
             $table->timestamps();
         });
     }
--- database/migrations/2024_08_06_131229_create_requirement_submissions_table.php
+++ database/migrations/2024_08_06_131229_create_requirement_submissions_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Requirements;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('requirement_submissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Requirements::class)->constrained()->onDelete('cascade');
+            $table->dateTime('date_pass');
             $table->timestamps();
         });
     }

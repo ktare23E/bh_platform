8209063399ed9e6e65f0aad3dd23512cc5345d37
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-08-07 08:41:34 +0800

    Done register section layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,6 +6,24 @@
                 <h1 class="text-3xl font-bold">Register</h1>
             </div>
             <div class="space-y-4">
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="space-y-2">
+                        <label
+                            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            for="email">First Name</label>
+                        <input
+                            class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            type="text" id="first_name" name="first_name" placeholder="John" required="" />
+                    </div>
+                    <div class="space-y-2">
+                        <label
+                            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            for="email">Last Name</label>
+                        <input
+                            class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            type="text" id="last_name" name="last_name" placeholder="Doe" required="" />
+                    </div>
+                </div>
                 <div class="space-y-2">
                     <label
                         class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"

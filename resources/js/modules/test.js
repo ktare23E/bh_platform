f6760a023ad21d3163a94280ff2b7caa53682164
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-08-07 12:49:15 +0800

    Requirement submission

--- resources/js/modules/test.js
+++ resources/js/modules/test.js
@@ -1,5 +1,6 @@
 export function dataTable(){
     $(document).ready( function () {
         $('#myTable').DataTable();
+        $('#myTable2').DataTable();
     } );
 }
--- resources/views/admin/requirements/index.blade.php
+++ resources/views/admin/requirements/index.blade.php
@@ -31,10 +31,31 @@
                         </tr>
                     </tbody>
                 </table>
-
-                
             </div>
-            
+            <h1 class="text-2xl font-bold mt-24">Requirement Submission List</h1>
+            <div class="mt-4 bg-white w-full p-[2rem] rounded-sm shadow-2xl transition-all">
+                <table id="myTable2" class="display">
+                    <thead>
+                        <tr>
+                            <th>Column 1</th>
+                            <th>Column 2</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Row 1 Data 1</td>
+                            <td>Row 1 Data 2</td>
+                            <td><a href="">view</a></td>
+                        </tr>
+                        <tr>
+                            <td>Row 2 Data 1</td>
+                            <td>Row 2 Data 2</td>
+                            <td><a href="" class="py-1 px-2 bg-gradient-to-tr from-[#2D2426] to-blue-400 text-white rounded-sm text-sm">view</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
     
